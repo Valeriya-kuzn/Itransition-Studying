93fd84241286e6fbba69c5891fc80ebd223005c2
valeriya kuzn <valeriyakuznetsova123@gmail.com>
2023-08-29 14:19:37 +0400

    update react-script, add backend file

--- project/src/components/Posts.jsx
+++ project/src/components/Posts.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Posts() {
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        setPost(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching post:', error);
+      });
+  }, []);
+
   return (
     <div>
-        Все посты
+      {post.map(item => (
+        <div className="post" key={item.id}>
+          <div className="posttitle">{item.title}</div>
+          <div className="postcontent">{item.body}</div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default Posts;
